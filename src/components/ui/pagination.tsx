61e2883bb4116531866863d1246bfcdcfb024551
shahank42 <shounakghosh42@gmail.com>
2024-09-14 19:01:28 +0530

    chore: Appeased biome

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -6,11 +6,12 @@ import {
 } from "@radix-ui/react-icons"
 
 import { cn } from "@/lib/utils"
-import { ButtonProps, buttonVariants } from "@/components/ui/button"
+import type { ButtonProps } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
-    role="navigation"
+    // role="navigation"
     aria-label="pagination"
     className={cn("mx-auto flex w-full justify-center", className)}
     {...props}
